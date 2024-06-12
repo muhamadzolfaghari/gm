4ca5d86e477cda96c08f518be9c3f6a47fb7e444
muhamad <muhamad.zolfaghari@gmail.com>
2024-06-12 22:27:29 +0330

    first commit

--- index.ts
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;   
